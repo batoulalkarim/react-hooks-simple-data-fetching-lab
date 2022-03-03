c4ba25f93ed18762d5b08f0c4854e57a6f9a17d9
Batoul Alkarim <batoulalkarim1@gmail.com>
2022-03-02 22:41:29 -0500

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
+import React, { useEffect, useState } from 'react';
+
 // create your App component here
+function App() {
+    const [image, setImage] = useState("")
+    
+
+useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(res => res.json())
+    .then((data) => {
+        setImage(data.message);
+       
+    })
+}, [])
+
+if (!image) return <p>Loading...</p>;
+
+return <img alt="A Random Dog" src={image}/>
+
+}
+
+export default App
